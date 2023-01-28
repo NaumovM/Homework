63f2e1cb4935c83af6f5d76112e229be5162293d
Naumov <naumovmk@yandex.ru>
2023-01-28 12:30:28 +0600

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+
+Console.Clear();
+Console.Write("Enter number: ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0) {
+    Console.WriteLine($"Number {a} is even");
+}
+else Console.WriteLine($"Number {a} is odd");
