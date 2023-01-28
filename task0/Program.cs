c32eab141a04565008a2b98feac99ab8b7680797
Naumov <naumovmk@yandex.ru>
2023-01-28 12:14:02 +0600

    add task0

---
+++ task0/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает 
+//два числа и выдает, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.Write("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+if(a > b) {
+    Console.WriteLine($"{a} > {b}");
+}
+else Console.WriteLine($"{b} > {a}");
